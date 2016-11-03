52cc134cecfc4bd9fea7cffbc9d58d988258b61d
bjxiaojian <bjxiaojian@corp.netease.com>
2016-11-03 21:43:30 +0800

    update

---
+++ src/main/java/com/example/myFirstProject/common/MyConstants.java
@@ -0,0 +1,8 @@
+package com.example.myFirstProject.common;
+
+/**
+ * 定义一些常量
+ */
+public class MyConstants {
+    public static final String USER_FORWARD_CACHE_PREFIX = "myboot:user";// user缓存前缀
+}
