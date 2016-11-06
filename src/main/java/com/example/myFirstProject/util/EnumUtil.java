d9a85b2dd4f9fedd7bd793973adba3fea1864529
bjxiaojian <hitxiaojian@163.com>
2016-11-07 01:16:04 +0800

    update

--- src/main/java/com/example/myFirstProject/util/EnumUtil.java
+++ src/main/java/com/example/myFirstProject/util/EnumUtil.java
@@ -5,15 +5,13 @@ import com.example.myFirstProject.service.EnumMessage;
 
 public class EnumUtil {
 
-
     /**
      * 获取value返回枚举对象
      * @param value
      * @param clazz
      * */
     public static <T extends EnumMessage>  T getEnumObject(Object value, Class<T> clazz){
-        return (T) Constant.ENUM_MAP.get(clazz).get(value);
+        return (T)Constant.ENUM_MAP.get(clazz).get(value);
     }
-
 
 }
